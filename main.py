095fd4080c9c070d4e406b43251d5b69cef1be9a
Hiendang7613 <73353492+hiendang7613@users.noreply.github.com>
2023-09-07 15:05:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from promptify import Prompter
 
 # read parameters
 parser = argparse.ArgumentParser()
-parser.add_argument('--directory_path', default='/content/CV/English/')
+parser.add_argument('--directory_path', default='data_dir/CV')
 parser.add_argument('--mode',  default='CV', help='CV or JD') 
 parser.add_argument('--model_name',  default='gpt-3.5-turbo') 
 parser.add_argument('--api_key',  default='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') 
